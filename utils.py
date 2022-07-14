e76986efb4a1c521dcfcaa56bfb2f2c849b5ddf3
John MacTavish <john@techsolx.com>
2022-07-14 14:25:41 +1200

    fix: remove extra return values,
        * remove ignored return value from detectDrawings, fx returns only 1
        * in utils.py remove ignored values cv2.findContours returns only 1
    
    Ref: OpenCV docs cv.findContours
    https://docs.opencv.org/4.x/d3/dc0/group__imgproc__shape.html#gadf1ad6a0b82947fa1fe3c3d497f260e0
    
    MIT-CW-012011 #Time 2.0h

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def show_inline(img):
         plt.xticks([]), plt.yticks([])
         plt.show()
 
-    
+
 def close_windows():
     '''Close popup window via 'ESC' key.'''
     while cv2.waitKey(200) & 0xFF != 27:
@@ -37,7 +37,7 @@ def close_windows():
 #############################
 #### Show Video
 #############################
-    
+
 def video(function):
     '''Displays video with modifiable frames.'''
     video = cv2.VideoCapture(video_port)
@@ -48,7 +48,7 @@ def video(function):
             function(frame)
     cv2.destroyAllWindows()
     cv2.waitKey(1)
-    
+
 def shrink(img, scale):
     '''Returns a shrinked image by the scale specified.'''
     return cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
@@ -56,7 +56,7 @@ def shrink(img, scale):
 #############################
 #### Display Coordinates
 #############################
-    
+
 def coordinates():
     '''Displays coordinates of the mouse as it moves.'''
     window = 'Coordinates'
@@ -90,7 +90,7 @@ def findEdgedImage(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(gray, 75, 200)
-    
+
 def findGreatestContour(contours):
     cnt = [-1, -1]
     for i in range(0, len(contours)):
@@ -101,24 +101,24 @@ def findGreatestContour(contours):
 def detectDrawings(img, color_range):
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(img_hsv, color_range[0], color_range[1])
-    _, contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     if contours is []:
         return img
     cnt = findGreatestContour(contours)
-    
+
     x,y,w,h = cv2.boundingRect(cnt)
     output = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
 
     for contour in contours:
         pt, r = cv2.minEnclosingCircle(cnt)
-    
+
     box = cv2.boxPoints(cv2.minAreaRect(cnt))
     box = np.int0(box)
     if abs(cv2.contourArea(box) - cv2.contourArea(cnt)) > 10000:
         print(cv2.contourArea(box), cv2.contourArea(cnt))
     else:
         cv2.drawContours(img,[box],0,(0,0,255),2)
-    return img 
+    return img
 
 
 #############################
@@ -135,7 +135,7 @@ def find_radius(contour):
     '''Returns the radius of the smallest circle around contour'''
     radius = cv2.minEnclosingCircle(contour)[1]
     return int(radius)
-  
+
 #############################
 #### HSV
 #############################
@@ -147,11 +147,11 @@ def hsv_select(filename):
     if w > 700:
         img = cv2.resize(img, (700, int(h*(700.0/w))))  # resize to width of 700
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    
+
     window_name = 'HSV Select'
-    cv2.imshow(window_name, img) 
+    cv2.imshow(window_name, img)
     hsv_min = np.array([0, 0, 0])
-    hsv_max = np.array([179, 255, 255])  
+    hsv_max = np.array([179, 255, 255])
     def callback(value):
         hsv_min[0] = cv2.getTrackbarPos('H_min', window_name)
         hsv_max[0] = cv2.getTrackbarPos('H_max', window_name)
@@ -163,14 +163,14 @@ def hsv_select(filename):
         img_masked = cv2.bitwise_and(img, img, mask=mask)
         cv2.putText(img_masked, 'HSV Lower: {}'.format(tuple(hsv_min)), (10, 35), 0, 0.75, (255, 255, 255), 2)
         cv2.putText(img_masked, 'HSV Upper: {}'.format(tuple(hsv_max)), (10, 70), 0, 0.75, (255, 255, 255), 2)
-        cv2.imshow(window_name, img_masked)    
-    # make the trackbar used for HSV masking 
-    cv2.createTrackbar('H_min', window_name, 0, 179, callback) 
-    cv2.createTrackbar('H_max', window_name, 0, 179, callback)   
-    cv2.createTrackbar('S_min', window_name, 0, 255, callback)   
-    cv2.createTrackbar('S_max', window_name, 0, 255, callback)  
-    cv2.createTrackbar('V_min', window_name, 0, 255, callback)  
-    cv2.createTrackbar('V_max', window_name, 0, 255, callback)  
+        cv2.imshow(window_name, img_masked)
+    # make the trackbar used for HSV masking
+    cv2.createTrackbar('H_min', window_name, 0, 179, callback)
+    cv2.createTrackbar('H_max', window_name, 0, 179, callback)
+    cv2.createTrackbar('S_min', window_name, 0, 255, callback)
+    cv2.createTrackbar('S_max', window_name, 0, 255, callback)
+    cv2.createTrackbar('V_min', window_name, 0, 255, callback)
+    cv2.createTrackbar('V_max', window_name, 0, 255, callback)
     # set initial trackbar values
     cv2.setTrackbarPos('H_min', window_name, 0)
     cv2.setTrackbarPos('H_max', window_name, 179)
@@ -186,10 +186,10 @@ def hsv_select(filename):
     cv2.destroyAllWindows()
     cv2.waitKey(1)
 
-    
+
 def hsv_select_live():
     '''Thresholds live video via HSV Trackbar values.'''
-    window_name = 'HSV Select Live' 
+    window_name = 'HSV Select Live'
     cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
     video = cv2.VideoCapture(video_port)
     hsv_min = np.array([0,0,0])
@@ -211,16 +211,16 @@ def hsv_select_live():
             h,w,ch = frame.shape
             cv2.putText(img_masked, 'HSV Lower: {}'.format(hsv_min), (10, 35), 0, 0.75, (255, 255, 255), 2)
             cv2.putText(img_masked, 'HSV Upper: {}'.format(hsv_max), (10, 70), 0, 0.75, (255, 255, 255), 2)
-            cv2.imshow(window_name, img_masked)  
+            cv2.imshow(window_name, img_masked)
     def callback(value):
-        update()  
-    # make the trackbar used for HSV masking    
-    cv2.createTrackbar('H_min', window_name, 0, 179, callback) 
-    cv2.createTrackbar('H_max', window_name, 0, 179, callback)   
-    cv2.createTrackbar('S_min', window_name, 0, 255, callback)   
-    cv2.createTrackbar('S_max', window_name, 0, 255, callback)  
-    cv2.createTrackbar('V_min', window_name, 0, 255, callback)  
-    cv2.createTrackbar('V_max', window_name, 0, 255, callback) 
+        update()
+    # make the trackbar used for HSV masking
+    cv2.createTrackbar('H_min', window_name, 0, 179, callback)
+    cv2.createTrackbar('H_max', window_name, 0, 179, callback)
+    cv2.createTrackbar('S_min', window_name, 0, 255, callback)
+    cv2.createTrackbar('S_max', window_name, 0, 255, callback)
+    cv2.createTrackbar('V_min', window_name, 0, 255, callback)
+    cv2.createTrackbar('V_max', window_name, 0, 255, callback)
     # set initial trackbar values
     cv2.setTrackbarPos('H_min', window_name, 0)
     cv2.setTrackbarPos('H_max', window_name, 179)
@@ -233,7 +233,7 @@ def hsv_select_live():
         update()
     cv2.destroyAllWindows()
     cv2.waitKey(1)
-    
+
 #############################
 #### Feature Detection
 #############################
@@ -241,29 +241,29 @@ def hsv_select_live():
 def find_object(img, img_q, total_matches, MIN_MATCH_COUNT, kp_img, kp_frame, m, good_matches, color, query_columns):
     '''
     Draws an outline around a detected objects given matches and keypoints.
-    
-    If enough matches are found, extract the locations of matched keypoints in both images. 
+
+    If enough matches are found, extract the locations of matched keypoints in both images.
     The matched keypoints are passed to find the 3x3 perpective transformation matrix.
-    Use transformation matrix to transform the corners of img to corresponding points in trainImage. 
+    Use transformation matrix to transform the corners of img to corresponding points in trainImage.
     Draw matches.
     '''
     if total_matches > MIN_MATCH_COUNT:
         src_pts = np.float32([ kp_img[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
         dst_pts = np.float32([ kp_frame[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
 
-        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0) 
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         matchesMask = mask.ravel().tolist()
 
         h,w,ch = img_q.shape
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
         dst = cv2.perspectiveTransform(pts,M)
-        
+
         dst[:,:,0] += query_columns
         img = cv2.polylines(img,[np.int32(dst)], True, color ,3, cv2.LINE_AA)
         dst = None
     else:
         print("Not enough matches are found - %d/%d" % (total_matches, MIN_MATCH_COUNT))
-        
+
 
 def draw_matches(img, frame, total_keypoints, matches, kp_img, kp_frame):
     '''FLANN based Matcher:
@@ -271,7 +271,7 @@ def draw_matches(img, frame, total_keypoints, matches, kp_img, kp_frame):
     '''
     # Number of successful matches
     total_matches = 0
-    
+
     # Need to draw only good matches, so create a mask
     matchesMask = [[0,0] for i in xrange(len(matches))]
 
@@ -282,20 +282,20 @@ def draw_matches(img, frame, total_keypoints, matches, kp_img, kp_frame):
             matchesMask[i]=[1,0]
             total_matches += 1
             good_matches.append(m)
-     
+
     # Getting percentages and putting it on screen
     match_percent = round(total_matches/total_keypoints, 3)
-    
+
     cv2.putText(frame,'Match:{}'.format(match_percent),(0, 100), 1, 4,(255,255,255), 1, cv2.LINE_AA)
-    
+
     # params for drawMatchesKnn
     draw_params = dict(matchColor = (0,255,0),
                    singlePointColor = (255,0,0),
                    matchesMask = matchesMask,
                    flags = 0)
-    
+
     img_matches = cv2.drawMatchesKnn(img, kp_img, frame, kp_frame, matches, None, **draw_params)
-    
+
     return img_matches, total_matches, m, good_matches
 
 #############################
@@ -322,31 +322,31 @@ def draw_hough_lines(img, lines):
                 y1 = int(y0 + 1000*(a))  # Y coordinate of point 1
                 x2 = int(x0 - 1000*(-b)) # X coordinate of point 2
                 y2 = int(y0 - 1000*(a))  # Y coordinate of point 2
-        
+
                 # Using the the 2 points defined above, call cv2.line() to draw the line on the image
                 cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
 #############################
 #### Checkerboards
-#############################                
+#############################
 
 def draw_checkerboard_center(img):
     # convert image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # locate checkerboard
-    ret, corn = cv2.findChessboardCorners(gray, (7,7), None) 
+    ret, corn = cv2.findChessboardCorners(gray, (7,7), None)
     x = 0
     y = 0
     # if the checkerboard is found ...
-    if ret == True:  
+    if ret == True:
         # draw checkerboard on img
-        img = cv2.drawChessboardCorners(img, (7,7), corn, ret) 
+        img = cv2.drawChessboardCorners(img, (7,7), corn, ret)
         x = corn[24,0 ,0]
         y = corn[24,0,1]
         # draw a circle arround the center of the checkerboard
-        cv2.circle(img,(x,y),30,(0,0,255), 3) 
+        cv2.circle(img,(x,y),30,(0,0,255), 3)
     # return the annotated img and the x coordinate of the center of the checkerboard
-    return img, x, y  
+    return img, x, y
 
 
-                
+
